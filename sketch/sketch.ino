4456b74333ebba128f48ba3e09ec7feb425e06a2
keanjapesan <keanjapesan@hotmail.com>
2015-05-24 14:13:05 -0600

    Refactor alarm code

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,20 +1,30 @@
-#define ALARM 3
- 
-float sinVal;
-int toneVal;
+#define ALARM 12
+
 void setup() {
   pinMode(ALARM, OUTPUT);
 }
- 
+
 void loop() {
-  for (int x=0; x<180; x++) {
-    // convert degrees to radians then obtain sin value
-    sinVal = (sin(x*(3.1412/180)));
-    // generate a frequency from the sin value
-    toneVal = 2000+(int(sinVal*1000));
-    tone(ALARM, toneVal);
+  if(false){
+    alarm();
   }
-       
-  delay(100); 
+  delay(10000);
 }
 
+void alarm(){
+  float sinVal;
+  int toneVal;
+
+  for (int i=0; i<30; i++) {
+    for (int x=0; x<180; x++) {
+      // convert degrees to radians then obtain sin value
+      sinVal = (sin(x*(3.1412/180)));
+      // generate a frequency from the sin value
+      toneVal = 2000+(int(sinVal*1000));
+      tone(ALARM, toneVal);
+    }
+    delay(100);
+  }
+
+  noTone(ALARM);
+}
