579a5760ba8d585447594dba3a73b860131a8108
keanjapesan <keanjapesan@hotmail.com>
2015-06-20 12:59:06 -0600

    Refactor alarm functions
    Also the flush logic was improved

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,5 @@
 #define ALARM 12
+#define LED 13
 String authorizedID = "190077761F07";
 String incomingData;   // for incoming serial data
 char stream;
@@ -7,6 +8,7 @@ void setup() {
   Serial1.begin(9600);     // opens serial port, sets data rate to 9600 bps
   Serial.begin(9600);
   pinMode(ALARM, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
@@ -22,17 +24,17 @@ void loop() {
     Serial.print("I received: ");
     Serial.println(incomingData);
     if(incomingData.indexOf(authorizedID)>=0){
-      Serial.print("Starting Alarm!!!");
+      Serial.println("Starting Alarm!!!");
       alarm();
-      delay(5000);
-      Serial.print("Stopped Alarm");
+      //delay(5000);
+      Serial.println("Stopped Alarm");
     }
     incomingData = "";
-    Serial1.flush();
+    flushReceive(); //Serial1.flush();
   }
 }
 
-void alarm(){
+void soundAlarm(){
   float sinVal;
   int toneVal;
 
@@ -49,3 +51,37 @@ void alarm(){
 
   noTone(ALARM);
 }
+
+void lightAlarm() {
+  for(int i=0; i<15; i++){
+    digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(100);              // wait for a second
+    digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
+    delay(100);              // wait for a second
+  }
+}
+
+void alarm(){
+  float sinVal;
+  int toneVal;
+
+  for (int i=0; i<30; i++) {
+    for (int x=0; x<180; x++) {
+      // convert degrees to radians then obtain sin value
+      sinVal = (sin(x*(3.1412/180)));
+      // generate a frequency from the sin value
+      toneVal = 2000+(int(sinVal*1000));
+      tone(ALARM, toneVal);
+    }
+    digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(100);              // wait for a second
+    digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
+  }
+
+  noTone(ALARM);
+}
+
+void flushReceive(){
+  while(Serial1.available()){ Serial1.read(); }
+}
+
